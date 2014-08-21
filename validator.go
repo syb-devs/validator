eed87d9e6179e7e6b6d2ded05edaa6f354455c12
Daniel Blanco <zareone2000@gmail.com>
2014-08-21 15:55:55 +0200

    Some doc and cleaning up

--- validator.go
+++ validator.go
@@ -20,12 +20,15 @@ var (
 	tagName          = "validation"
 )
 
+// Rule represents a validation rule that will be applied to a struct field value.
 type Rule interface {
 	Validate(data interface{}, field string, params map[string]string) error
 }
 
+// errList is used to store struct validation errors grouped by field name.
 type errList map[string][]error
 
+// String returns a literal representation of the error list.
 func (e errList) String() string {
 	str := ""
 	for field, errors := range e {
@@ -38,10 +41,12 @@ func (e errList) String() string {
 	return str
 }
 
+// Len returns the number of elements in the error list.
 func (e errList) Len() int {
 	return len(e)
 }
 
+// ruleMap stores validation rules that will be accessed by its name.
 type ruleMap map[string]Rule
 
 type validator struct {
@@ -53,18 +58,23 @@ type validator struct {
 	fieldPrefix     string
 }
 
+// RegisterRule registers a validation rule in the default validator.
 func RegisterRule(name string, rule Rule) {
 	defaultValidator.RegisterRule(name, rule)
 }
 
+// Validate validates the given struct using the default validator and returns any logic error that might happen.
+// To get the actual validation errors, use the method Errors().
 func Validate(data interface{}) error {
 	return defaultValidator.Validate(data)
 }
 
-func TagName(name string) {
+// SetTagName sets the tag name used to exctact validation rules from the struct field.
+func SetTagName(name string) {
 	tagName = name
 }
 
+// New returns a new validator, set up with the default rules and options.
 func New() *validator {
 	v := &validator{
 		registeredRules: make(ruleMap, 0),
@@ -75,12 +85,14 @@ func New() *validator {
 	return v
 }
 
+// RegisterRule registers a Rule in for this validator under the given name.
 func (v *validator) RegisterRule(name string, rule Rule) {
 	v.mu.Lock()
 	defer v.mu.Unlock()
 	v.registeredRules[name] = rule
 }
 
+// getRule retrieves a rule from the rule map using a given name.
 func (v *validator) getRule(name string) (Rule, error) {
 	v.mu.RLock()
 	defer v.mu.RUnlock()
@@ -91,10 +103,14 @@ func (v *validator) getRule(name string) (Rule, error) {
 	return nil, ErrRuleNotFound
 }
 
+// setFieldPrefix sets the literal used to prefix fields of nested structs.
 func (v *validator) setFieldPrefix(prefix string) {
 	v.fieldPrefix = prefix
 }
 
+// Validate runs the actual validation of the struct, applying the rules registered in the validator, returning
+// any logic error that might happen.
+// To get the actual validation errors, use the method Errors().
 func (v *validator) Validate(data interface{}) error {
 	sv := reflect.ValueOf(data)
 	if sv.Kind() == reflect.Ptr && !sv.IsNil() {
@@ -116,6 +132,7 @@ func (v *validator) Validate(data interface{}) error {
 	return nil
 }
 
+// validateField validates a single field of the struct and returns a logic error if something goes wrong.
 func (v *validator) validateField(i int) error {
 
 	elem := reflect.TypeOf(v.data).Field(i)
@@ -181,6 +198,7 @@ func (v *validator) validateField(i int) error {
 	return nil
 }
 
+// Errors returns a list of validation errors.
 func (v *validator) Errors() *errList {
 	errors := v.errors
 	if len(errors) == 0 {
@@ -189,6 +207,7 @@ func (v *validator) Errors() *errList {
 	return &errors
 }
 
+// ErrorsByField returns a list of validation errors for a given field.
 func (v *validator) ErrorsByField(field string) *[]error {
 	if field == "" {
 		return nil
@@ -203,6 +222,7 @@ func (v *validator) ErrorsByField(field string) *[]error {
 
 type safeFunc func()
 
+// safeExec executes a given function and stores any recovered panic as a logic error inside de validator.
 func (v *validator) safeExec(f safeFunc) {
 	defer func() {
 		if recErr := recover(); recErr != nil {
@@ -219,6 +239,7 @@ func (v *validator) safeExec(f safeFunc) {
 	f()
 }
 
+// IsStruct checks if the given value is a struct of a pointer to a struct.
 func IsStruct(data interface{}) bool {
 	v := reflect.ValueOf(data)
 	if v.Kind() == reflect.Ptr {
@@ -227,19 +248,18 @@ func IsStruct(data interface{}) bool {
 	return v.Kind() == reflect.Struct
 }
 
+// fieldIsExported  returns true if the struct field is exported.
 func fieldIsExported(f reflect.StructField) bool {
 	return len(f.PkgPath) == 0
 }
 
+// getInterfaceValue returns the value of a given interface using reflection.
 func getInterfaceValue(data interface{}, name string) interface{} {
 	return reflect.ValueOf(data).FieldByName(name).Interface()
 }
 
-func ruleString(ruleName, structField string, data interface{}) string {
-	return fmt.Sprintf("<<Validation Rule: %s. Field: %s. Data: %s>>", ruleName, structField, fmt.Sprintf("%+v", data))
-}
-
-// toString
+// toString returns a literal representation of a given value.
+// The second parameter indicates whether a conversion was possible or not.
 func toString(value interface{}) (string, bool) {
 	switch v := value.(type) {
 	case string, *string, int, *int, int32, *int32, int64, *int64:
@@ -249,6 +269,7 @@ func toString(value interface{}) (string, bool) {
 	}
 }
 
+// mustStringify tries to convert the given value to string type and panics if not possible.
 func mustStringify(value interface{}) string {
 	strVal, ok := toString(value)
 	if ok == false {
